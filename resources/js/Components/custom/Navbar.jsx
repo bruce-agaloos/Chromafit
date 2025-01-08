3aa4b900f1e2b1e85093f1880861ea09fe7cabbf
bakasibruceto <abla09027@gmail.com>
2025-01-09 04:20:49 +0800

    burger

--- resources/js/Components/custom/Navbar.jsx
+++ resources/js/Components/custom/Navbar.jsx
@@ -3,6 +3,7 @@ import { Link } from "@inertiajs/react";
 
 const Navbar = ({ auth }) => {
     const [scrollY, setScrollY] = useState(0);
+    const [sidebarOpen, setSidebarOpen] = useState(false);
 
     useEffect(() => {
         const handleScroll = () => {
@@ -14,6 +15,10 @@ const Navbar = ({ auth }) => {
             window.removeEventListener("scroll", handleScroll);
         };
     }, []);
+
+    const toggleSidebar = () => {
+        setSidebarOpen(!sidebarOpen);
+    };
 
     const topValue = Math.max(0, 2.5 - scrollY / 15.5); // Adjust the divisor to control the speed of the transition
 
@@ -28,16 +33,41 @@ const Navbar = ({ auth }) => {
                 style={{ top: `${topValue}rem`, height: "8.3rem" }}
             >
                 <div className="grid grid-cols-3 items-center gap-2 py-10 lg:grid-cols-3">
-                    <div></div>
-                    <div className=" -mt-5 text-5xl font-londrina flex lg:col-start-2 justify-center">
+                    <button
+                        className={`lg:hidden ml-7 w-10 ${
+                            scrollY > 10 ? "text-black" : "text-white"
+                        }`}
+                        onClick={toggleSidebar}
+                    >
+                        <svg
+                            className="w-8 h-8"
+                            fill="none"
+                            stroke="currentColor"
+                            viewBox="0 0 24 24"
+                            xmlns="http://www.w3.org/2000/svg"
+                        >
+                            <path
+                                strokeLinecap="round"
+                                strokeLinejoin="round"
+                                strokeWidth="2"
+                                d="M4 6h16M4 12h16m-7 6h7"
+                            ></path>
+                        </svg>
+                    </button>
+                    <div className="hidden lg:flex text-5xl font-londrina lg:col-start-2 justify-center -mt-5">
                         Chromafit
                     </div>
-                    <nav className="-mx-3 -mt-2 flex flex-1 justify-end mr-10 font-montserrat text-xs lg:col-start-3">
+                    <div className="lg:hidden flex text-5xl font-londrina lg:col-start-2 justify-center">
+                        Chromafit
+                    </div>
+                    <nav className="hidden lg:flex -mx-3 -mt-2 flex-1 justify-end mr-10 font-montserrat text-xs lg:col-start-3">
                         {auth.user ? (
                             <Link
                                 href={route("profile")}
                                 className={`rounded-md px-3 py-2 ring-1 ring-transparent transition hover:text-black/70 focus:outline-none focus-visible:ring-[#FF2D20] ${
-                                    scrollY > 10 ? "text-slate-500" : "text-white"
+                                    scrollY > 10
+                                        ? "text-slate-500"
+                                        : "text-white"
                                 }`}
                             >
                                 ACCOUNT
@@ -74,7 +104,7 @@ const Navbar = ({ auth }) => {
                         </Link>
                     </nav>
                 </div>
-                <nav className="flex justify-center items-center font-montserrat text-sm -mt-5">
+                <nav className="hidden lg:flex justify-center items-center font-montserrat text-sm -mt-5">
                     <Link
                         href="#"
                         className={`rounded-md px-3 py-2 ring-1 ring-transparent transition hover:text-black/70 focus:outline-none focus-visible:ring-[#FF2D20] ${
@@ -120,6 +150,81 @@ const Navbar = ({ auth }) => {
                     </Link>
                 </nav>
             </header>
+            <div
+                className={`fixed inset-0 bg-black bg-opacity-50 z-40 transition-opacity duration-300 ${
+                    sidebarOpen
+                        ? "opacity-100"
+                        : "opacity-0 pointer-events-none"
+                }`}
+                onClick={toggleSidebar}
+            ></div>
+            <div
+                className={`fixed inset-y-0 left-0 bg-white z-50 transform transition-transform duration-300 ${
+                    sidebarOpen ? "translate-x-0" : "-translate-x-full"
+                }`}
+                style={{ width: "50%" }}
+            >
+                <div className="p-4">
+                    <button className="text-black" onClick={toggleSidebar}>
+                        <svg
+                            className="w-6 h-6"
+                            fill="none"
+                            stroke="currentColor"
+                            viewBox="0 0 24 24"
+                            xmlns="http://www.w3.org/2000/svg"
+                        >
+                            <path
+                                strokeLinecap="round"
+                                strokeLinejoin="round"
+                                strokeWidth="2"
+                                d="M6 18L18 6M6 6l12 12"
+                            ></path>
+                        </svg>
+                    </button>
+                </div>
+                <nav className="flex flex-col p-4 space-y-4 font-montserrat text-sm">
+                    <Link
+                        href="#"
+                        className={`rounded-md px-3 py-2 ring-1 ring-transparent transition hover:text-black/70 focus:outline-none focus-visible:ring-[#FF2D20] ${
+                            scrollY > 10 ? "text-black" : "text-white"
+                        }`}
+                    >
+                        NEW ARRIVAL
+                    </Link>
+                    <Link
+                        href="#"
+                        className={`rounded-md px-3 py-2 ring-1 ring-transparent transition hover:text-black/70 focus:outline-none focus-visible:ring-[#FF2D20] ${
+                            scrollY > 10 ? "text-black" : "text-white"
+                        }`}
+                    >
+                        ALL PRODUCTS
+                    </Link>
+                    <Link
+                        href="#"
+                        className={`rounded-md px-3 py-2 ring-1 ring-transparent transition hover:text-black/70 focus:outline-none focus-visible:ring-[#FF2D20] ${
+                            scrollY > 10 ? "text-black" : "text-white"
+                        }`}
+                    >
+                        COLLABORATIONS
+                    </Link>
+                    <Link
+                        href="#"
+                        className={`rounded-md px-3 py-2 ring-1 ring-transparent transition hover:text-black/70 focus:outline-none focus-visible:ring-[#FF2D20] ${
+                            scrollY > 10 ? "text-black" : "text-white"
+                        }`}
+                    >
+                        MEN
+                    </Link>
+                    <Link
+                        href="#"
+                        className={`rounded-md px-3 py-2 ring-1 ring-transparent transition hover:text-black/70 focus:outline-none focus-visible:ring-[#FF2D20] ${
+                            scrollY > 10 ? "text-black" : "text-white"
+                        }`}
+                    >
+                        WOMEN
+                    </Link>
+                </nav>
+            </div>
         </>
     );
 };
