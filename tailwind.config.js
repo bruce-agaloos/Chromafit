9a4174472f8a778e3ef352c48758c0f096a552f8
bakasibruceto <abla09027@gmail.com>
2025-01-09 01:38:22 +0800

    custom animations

--- tailwind.config.js
+++ tailwind.config.js
@@ -14,6 +14,33 @@ export default {
         extend: {
             fontFamily: {
                 sans: ['Figtree', ...defaultTheme.fontFamily.sans],
+                londrina: ['Londrina Solid', 'sans-serif'],
+                montserrat: ['Montserrat', 'sans-serif'],
+            },
+            keyframes: {
+                fadeInUp: {
+                    '0%': { opacity: 0, transform: 'translateY(40px)' },
+                    '100%': { opacity: 1, transform: 'translateY(0)' },
+                },
+                fadeOutDown: {
+                    '0%': { opacity: 1, transform: 'translateY(0)' },
+                    '100%': { opacity: 0, transform: 'translateY(60px)' },
+                },
+
+                buttonInUp: {
+                    '0%': { opacity: 0, transform: 'translateY(60px)' },
+                    '100%': { opacity: 1, transform: 'translateY(0)' },
+                },
+                buttonOutDown: {
+                    '0%': { opacity: 1, transform: 'translateY(0)' },
+                    '100%': { opacity: 0, transform: 'translateY(70px)' },
+                },
+            },
+            animation: {
+                fadeInUp: 'fadeInUp 1s forwards',
+                fadeOutDown: 'fadeOutDown 1s forwards',
+                buttonInUp: 'buttonInUp 1s forwards',
+                buttonOutDown: 'buttonOutDown 1s forwards',
             },
         },
     },
