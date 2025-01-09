a37e8f2decfc88687d3de027b8601850cadbb8e9
bakasibruceto <abla09027@gmail.com>
2025-01-09 18:25:15 +0800

    boxShadow

--- tailwind.config.js
+++ tailwind.config.js
@@ -35,13 +35,23 @@ export default {
                     '0%': { opacity: 1, transform: 'translateY(0)' },
                     '100%': { opacity: 0, transform: 'translateY(70px)' },
                 },
+
+                upDown: {
+                    '0%, 100%': { transform: 'translateY(3px)' },
+                    '50%': { transform: 'translateY(-10px)' },
+                  },
             },
             animation: {
                 fadeInUp: 'fadeInUp 1s forwards',
                 fadeOutDown: 'fadeOutDown 1s forwards',
                 buttonInUp: 'buttonInUp 1s forwards',
                 buttonOutDown: 'buttonOutDown 1s forwards',
+                upDown: 'upDown 2s ease-in-out infinite',
             },
+            boxShadow: {
+                glow: '0 0 10px rgba(255, 255, 255, 0.8)',
+                glowBrighter: '0 0 20px rgba(255, 255, 255, 1)',
+              },
         },
     },
 
